987af0bd723b12a2e1a5ff93d9fd338dcf9571e6
Salwa-BH <48294041+Salwa-BH@users.noreply.github.com>
2019-04-22 16:45:12 +0100

    Add files via upload

---
+++ TP3.table-cards.js
@@ -0,0 +1,301 @@
+var users =[
+    {
+        "gender": "female",
+        "name": {
+            "title": "miss",
+            "first": "léa",
+            "last": "roger"
+        },
+        "email": "léa.roger@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/women/25.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/women/25.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/25.jpg"
+        }
+    },
+    {
+        "gender": "female",
+        "name": {
+            "title": "miss",
+            "first": "océane",
+            "last": "andre"
+        },
+        "email": "océane.andre@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/women/4.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/women/4.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/4.jpg"
+        }
+    },
+    {
+        "gender": "female",
+        "name": {
+            "title": "miss",
+            "first": "lilou",
+            "last": "robin"
+        },
+        "email": "lilou.robin@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/women/48.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/women/48.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/48.jpg"
+        }
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "soan",
+            "last": "berger"
+        },
+        "email": "soan.berger@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/58.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/58.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/58.jpg"
+        }
+    },
+    {
+        "gender": "female",
+        "name": {
+            "title": "mrs",
+            "first": "leana",
+            "last": "rousseau"
+        },
+        "email": "leana.rousseau@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/women/50.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/women/50.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/50.jpg"
+        }
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "ethan",
+            "last": "deschamps"
+        },
+        "email": "ethan.deschamps@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/0.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/0.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/0.jpg"
+        }
+    },
+    {
+        "gender": "female",
+        "name": {
+            "title": "mrs",
+            "first": "emmy",
+            "last": "martin"
+        },
+        "email": "emmy.martin@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/women/68.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/women/68.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/68.jpg"
+        }
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "marin",
+            "last": "bernard"
+        },
+        "email": "marin.bernard@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/53.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/53.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/53.jpg"
+        }
+    },
+    {
+        "gender": "female",
+        "name": {
+            "title": "ms",
+            "first": "séléna",
+            "last": "leclerc"
+        },
+        "email": "séléna.leclerc@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/women/47.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/women/47.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/47.jpg"
+        }
+    },
+    {
+        "gender": "female",
+        "name": {
+            "title": "mrs",
+            "first": "amelia",
+            "last": "pierre"
+        },
+        "email": "amelia.pierre@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/women/34.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/women/34.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/34.jpg"
+        }
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "alexandre",
+            "last": "hubert"
+        },
+        "email": "alexandre.hubert@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/33.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/33.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/33.jpg"
+        }
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "elias",
+            "last": "robert"
+        },
+        "email": "elias.robert@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/45.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/45.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/45.jpg"
+        }
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "milan",
+            "last": "gauthier"
+        },
+        "email": "milan.gauthier@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/66.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/66.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/66.jpg"
+        }
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "nino",
+            "last": "legrand"
+        },
+        "email": "nino.legrand@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/51.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/51.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/51.jpg"
+        }
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "noham",
+            "last": "nguyen"
+        },
+        "email": "noham.nguyen@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/99.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/99.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/99.jpg"
+        }
+    },
+    {
+        "gender": "female",
+        "name": {
+            "title": "mrs",
+            "first": "louane",
+            "last": "noel"
+        },
+        "email": "louane.noel@example.com",
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/women/32.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/women/32.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/32.jpg"
+        }
+    }
+]
+
+function showTable() {
+    var table = "<table class='table' >";
+    var boutton='<div class="row" style="text-align:center; background-color: #ebfa95 ;"><br> <div class="col-md-6"><button type="button" class="btn btn-success" onclick="showMale()">Show Male only</button></div><div class="col-md-6"><button type="button" class="btn btn-success" onclick="showFemale()">Show Female only</button></div><br><br></div>'
+    table += "<tr><th>Photo</th><th>title</th><th>Nom</th><th>Prénom</th><th>email</th><th>Show card</th></tr>";
+    for (var i=0; i<users.length; i++){
+        table+='<tr>';
+        table+='<td>' + '<img src="'+users[i].picture.large + '"></td>';
+        table+='<td>'+ users[i].name.title +'</td>';
+        table+='<td>'+ users[i].name.last +'</td>';
+        table+='<td>'+ users[i].name.first +'</td>';
+        table+='<td>'+ users[i].email +'</td>';
+        
+        table+='<td>'+'<button type="button" class="btn-more">More</button>'+'</td>';
+        table+='</tr>';
+    }
+    table += "</table>";
+    document.querySelector('#out').innerHTML=boutton+table;
+}
+function showMale(){
+    //let adults = users.filter(title => if(title="mr") title);
+    var table = "<table class='table' >";
+    var boutton='<div class="row" style="text-align:center; background-color: #ebfa95 ;"><br> <div class="col-md-4"><button type="button" class="btn btn-success" onclick="showMale()">Show Male only</button></div><div class="col-md-4"><button type="button" class="btn btn-success" onclick="showTable()">Show Male and Female</button></div><div class="col-md-4"><button type="button" class="btn btn-success" onclick="showFemale()">Show Female only</button></div><br><br></div>'
+    table += "<tr><th>Photo</th><th>title</th><th>Nom</th><th>Prénom</th><th>email</th><th>Show card</th></tr>";
+    for (var i=0; i<users.length; i++){
+        if(users[i].name.title=="mr"){
+            table+='<tr>';
+            table+='<td>' + '<img src="'+users[i].picture.large + '"></td>';
+            table+='<td>'+ users[i].name.title +'</td>';
+            table+='<td>'+ users[i].name.last +'</td>';
+            table+='<td>'+ users[i].name.first +'</td>';
+            table+='<td>'+ users[i].email +'</td>';
+            
+            table+='<td>'+'<button type="button" class="btn-more">More</button>'+'</td>';
+            table+='</tr>';
+        }
+    }
+    table += "</table>";
+    document.querySelector('#out').innerHTML=boutton+table;
+}
+function showFemale(){
+    //let adults = users.filter(title => if(title="mr") title);
+    var table = "<table class='table' >";
+    var boutton='<div class="row" style="text-align:center; background-color: #ebfa95 ;"><br> <div class="col-md-4"><button type="button" class="btn btn-success" onclick="showMale()">Show Male only</button></div><div class="col-md-4"><button type="button" class="btn btn-success" onclick="showTable()">Show Male and Female</button></div><div class="col-md-4"><button type="button" class="btn btn-success" onclick="showFemale()">Show Female only</button></div><br><br></div>'
+    table += "<tr><th>Photo</th><th>title</th><th>Nom</th><th>Prénom</th><th>email</th><th>Show card</th></tr>";
+    for (var i=0; i<users.length; i++){
+        if(users[i].name.title=="mrs" || users[i].name.title=="miss"){
+            table+='<tr>';
+            table+='<td>' + '<img src="'+users[i].picture.large + '"></td>';
+            table+='<td>'+ users[i].name.title +'</td>';
+            table+='<td>'+ users[i].name.last +'</td>';
+            table+='<td>'+ users[i].name.first +'</td>';
+            table+='<td>'+ users[i].email +'</td>';
+            
+            table+='<td>'+'<button type="button" class="btn-more">More</button>'+'</td>';
+            table+='</tr>';
+        }
+    }
+    table += "</table>";
+    document.querySelector('#out').innerHTML=boutton+table;
+}
+function showCards() {
+    var card='<div class="row rowcard"><br><br>';
+    for (var i=0; i<users.length; i++){
+        card+='<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 centring"><div class="card" style="background-color: #a6c208">';
+        card+='<img class="card-img-top" src="'+users[i].picture.large + '" alt="Card image cap">';
+        card+='<div class="card-body">'
+        card+='<h5 class="card-title">'+ users[i].name.title+' '+users[i].name.first+' '+users[i].name.last+' '+'</h5>';
+        card+='<p class="card-text">Email: '+users[i].email +'</p>';
+        card+='<a href="#" class="btn btn-success">Show More</a>';
+        card+='</div></div><br></div>';
+    }
+    card+='</div>';
+    document.querySelector('#out').innerHTML=card;
+}
